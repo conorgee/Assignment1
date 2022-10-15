86b30841738555ea3e41e1d72316dfdd0e430927
conorgee <conorgilmartin13@gmail.com>
2022-10-15 22:35:50 +0100

    m

--- lib/src/test/java/assignment1/test.java
+++ lib/src/test/java/assignment1/test.java
@@ -14,7 +14,7 @@ class test {
 	
 
 	@Test
-	void testSetUP() {
+	void testStudent() {
   		DateTime start1 = new DateTime(2013,8,14,0,0,0);
   		DateTime end1 = new DateTime(2013,8,16,0,0,0);
   		List<Student> students = new ArrayList<Student>();
@@ -52,22 +52,6 @@ class test {
 
 		assertEquals("Jim McGuinness65",jim.getUsername());
 	}
-		void testss()
-		{DateTime start1 = new DateTime(2013,8,14,0,0,0);
-		DateTime end1 = new DateTime(2013,8,16,0,0,0);
-		List<Student> students = new ArrayList<Student>();
-		List<Module> modules = new ArrayList<Module>();
-		List<Lecturer> lecturers = new ArrayList<Lecturer>();
-		List<CourseProgramme> courses = new ArrayList<CourseProgramme>();
-		CourseProgramme literature = new CourseProgramme("literature",modules,students,start1,end1);
-		courses.add(literature);
-		Student john = new Student("John McGuinness", 23,"22/6/1999", 1, courses);
-		Lecturer jim = new Lecturer("Jim McGuinness", 65,"22/6/1945", 1, courses);
-		Module english = new Module("english",students, lecturers,courses);
-		modules.add(english);
-		students.add(john);
-		lecturers.add(jim);	}
-		
 
 
 }
